2308a716fe2eebdbb3ea619e5490c936ee261c47
Ki4ri3 <lewiskiarie9@gmail.com>
2026-03-20 16:48:46 +0300

    newapp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Signup from './components/Signup';
+import Signin from './components/Signin';
+import Addproducts from './components/Addproducts';
+import Getproducts from './components/Getproducts';
+import Makepayment from './components/Makepayment';
+import Notfound from './components/Notfound';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Welcome To Digital Farm Market</h2>
       </header>
+
+      <Routes>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/signin' element={<Signin/>}/>
+        <Route path='/addproducts' element={<Addproducts/>}/>
+        <Route path='/' element={<Getproducts/>}/>
+        <Route path='/makepayment' element={<Makepayment/>}/>
+        <Route path='*' element={<Notfound/>}/>
+      </Routes>
     </div>
+    </Router>
   );
 }
 
